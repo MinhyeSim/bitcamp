7b321aa3fea87eb274eebc2062037be2028b8595
MinhyeSim <smine0032@gmail.com>
2022-01-25 11:10:11 +0900

    2022-01-25 11:10 최초 커밋

--- java/com/example/demo/calc/CalcDemo.java
+++ java/com/example/demo/calc/CalcDemo.java
@@ -8,8 +8,10 @@ public class CalcDemo {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         CalcApp calcApp = new CalcApp();
-        System.out.println();
-
+        System.out.println("첫번째 숫자 : ");
+        int num1 = scanner.nextInt();
+        System.out.println("연산자");
+        
 
     }
 
