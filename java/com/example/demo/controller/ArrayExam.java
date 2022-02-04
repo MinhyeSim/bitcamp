9b57f5021d3c99a0a2430fdca46b52f8bdf4d9c6
MinhyeSim <smine0032@gmail.com>
2022-02-04 17:55:45 +0900

    17:55

--- java/com/example/demo/controller/ArrayExam.java
+++ java/com/example/demo/controller/ArrayExam.java
@@ -19,6 +19,13 @@ public class ArrayExam {
                 " " ,     " " ,   "정렬",  "   ", "그래프"};
 
         System.out.println("Q1.팀별 과제를 출력하세요");
+        String s ="";
+        for(int i=0; i < arr.length;i++){
+            s += arr[i]+"\t";
+
+        }
+        System.out.println(s);
+
 
         System.out.println("Q2. 팀장이 맡은 과제만 출력하세요.");
 
--- java/com/example/demo/controller/TempController.java
+++ java/com/example/demo/controller/TempController.java
@@ -17,19 +17,19 @@ import java.util.Scanner;
  */
 public class TempController {
     public static void main(String[] args) {
-        System.out.println("Q1.팀별 과제를 출력하세요");
+
         String[] arr = {"심민혜", "권솔이", "김지혜", "하진희", "최은아",
                         "해시",    "힙",   "깊이",  "탐욕법", "스택",
                         "DP",   "완전탐색","너비", "이분탐색", "큐",
                         " " ,     " " ,   "정렬",  "   ", "그래프"};
-
+        System.out.println("Q1.팀별 과제를 출력하세요");
         String s = "";
         for (int i = 0; i < arr.length; i++) {
             if (i % 5 ==0) {s+="\n";}
              s += i + ":" + arr[i] ;
-
         }
             System.out.println(s);
+
         System.out.println("Q2. 팀장이 맡은 과제만 출력하세요.");
                 s = " ";
                 for(int i=0; i< arr.length; i++){
@@ -51,9 +51,7 @@ public class TempController {
 
                 }
                 System.out.println(s);
-                s = "";
-
-
+                
         System.out.println("Q3. 큐를 담당한 사람을 출력하세요 예) 큐를 담당한 사람 : 최은아");
         for(int i=0; i<arr.length; i++){
             if(arr[i].equals("큐")) {
