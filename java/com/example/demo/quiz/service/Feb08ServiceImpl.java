09e4484e76595b18f8a9e20db11676a8b64e7cb5
MinhyeSim <smine0032@gmail.com>
2022-02-09 11:24:41 +0900

    11:24 (수정)스터디과제 : bank

--- java/com/example/demo/quiz/service/Feb08ServiceImpl.java
+++ java/com/example/demo/quiz/service/Feb08ServiceImpl.java
@@ -40,7 +40,7 @@ public class Feb08ServiceImpl implements Feb08Service{
 
         while (true){
 
-            System.out.println("0)exit 1)입금 2)출금 3)잔고");
+            System.out.println("1)입금 2)출금 3)잔고 4)exit");
             System.out.println("선택 : ");
 
             int choice =  scanner.nextInt();
@@ -67,7 +67,7 @@ public class Feb08ServiceImpl implements Feb08Service{
 
                 case 4:
                     System.out.println("프로그램 종료");
-                    break;
+                    return;
 
                 default:
                     System.out.println("다시 입력해주세요");
